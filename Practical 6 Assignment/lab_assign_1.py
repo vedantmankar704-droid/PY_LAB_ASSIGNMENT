f204def6c3334052aa9f34f01fed045f6f1aa969
DELL <vedantmankar704@gmail.com>
2026-02-28 11:55:38 +0530

    .

---
+++ Practical 6 Assignment/lab_assign_1.py
@@ -0,0 +1,24 @@
+# Take string input from user
+text = input("Enter a string: ")
+
+vowels = 0
+consonants = 0
+spaces = 0
+lowercase = 0
+
+for ch in text:
+    if ch in "aeiouAEIOU":
+        vowels += 1
+    elif ch.isalpha():
+        consonants += 1
+
+    if ch == " ":
+        spaces += 1
+
+    if ch.islower():
+        lowercase += 1
+
+print("Number of Vowels:", vowels)
+print("Number of Consonants:", consonants)
+print("Number of Spaces:", spaces)
+print("Number of Lowercase letters:", lowercase)
