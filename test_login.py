a6ca61584a7d3dbdc22721be4a45479f040a83e2
Vedant Mankar <vedantmankar704@gmail.com>
2026-03-06 19:15:06 +0530

    Delete test_login.py

--- test_login.py
+++
@@ -1,6 +0,0 @@
-import json, urllib.request
-
-url = 'http://localhost:5000/api/user-login'
-data = json.dumps({'voter_id': 'user123', 'password': 'user123'}).encode()
-req = urllib.request.Request(url, data, headers={'Content-Type': 'application/json'})
-print(urllib.request.urlopen(req).read().decode())
