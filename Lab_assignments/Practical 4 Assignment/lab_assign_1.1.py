abd6205c0037413477e33ebee00287a78be302d0
ManthanKubade <kubademanthan@gmail.com>
2026-04-02 10:51:17 +0530

    Update from Copilot push request

--- Lab_assignments/Practical 4 Assignment/lab_assign_1.1.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-a = np.eye(4)
-print(a)
--- Lab_assignments/Practical 6 Assignment/lab_assign_2.py
+++
@@ -1,7 +0,0 @@
-# Function to convert lines to capital letters
-
-def capital_lines():
-    text = input("Enter a sentence: ")
-    print(text.upper())
-
-capital_lines()
